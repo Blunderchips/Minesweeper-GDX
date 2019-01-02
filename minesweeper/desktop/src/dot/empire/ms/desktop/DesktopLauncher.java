4d6e316c61e20d3111d16100c6026a870650663f
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-01-02 12:02:51 +0200

    Generated libGDX project

---
+++ minesweeper/desktop/src/dot/empire/ms/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dot.empire.ms.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dot.empire.ms.Minesweeper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Minesweeper(), config);
+	}
+}
